7c9cb5d74e167d0a5049cf94121e00d90c917ee8
R.Dhanush <144353154+Dhanushremo@users.noreply.github.com>
2025-03-26 22:36:07 +0530

    Update _1HallowDaimondPattren.java

--- src/com/dhanush/task/_1HallowDaimondPattren.java
+++ src/com/dhanush/task/_1HallowDaimondPattren.java
@@ -8,6 +8,7 @@ public static void main(String[] args) {
 	System.out.println("Enter the n value");
 	int n=new Scanner(System.in).nextInt();
 	for(int i=1;i<=n;i++) {
+		//For Spaces
 		for(int k=1;k<=n-i;k++) {
 			System.out.print(" ");
 		}
